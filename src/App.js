910db13cb358f37b64d08d26d8549a3d1870aca0
Akahad1 <aksahad234567@gmail.com>
2024-01-25 19:26:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navber from './Componet/Navber/Navber';
+
+function App() {
+  return (
+    <div className='  min-h-screen'>
+      <Navber></Navber>
+    
+    </div>
+  );
+}
+
+export default App;
