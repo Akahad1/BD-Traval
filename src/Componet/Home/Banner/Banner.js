d74a8dd5b20600a53543288cb067f0b34c7cb8d3
Akahad1 <aksahad234567@gmail.com>
2024-02-02 20:15:17 +0600

    8th commit

--- src/Componet/Home/Banner/Banner.js
+++ src/Componet/Home/Banner/Banner.js
@@ -2,7 +2,16 @@ import React from "react";
 
 const Banner =()=>{
     return(<div>
-        
+        <div className='bg-[#e1e1e1]  flex justify-center lg:pb-32 md:pb-32 lg:pt-24 md:pt-24 pb-20 pt-16'>
+      <div>
+      <p className='text-xl mb-5 lg:ml-14 md:ml-12 ml-5 font-serif'>Destination</p>
+      <p className='lg:text-5xl md:text-5xl text-3xl font-sans'>Bangladesh</p>
+
+      </div>
+      
+
+
+    </div>
 
     </div>)
 }
